5e1625414180d0d819771380ead474877736a41e
Sarvesh Khetan <khetanmamta@gmail.com>
2025-12-21 22:15:29 -0500

    knapsack updated

--- array/recurssion/knapsack.py
+++ array/recurssion/knapsack.py
@@ -8,16 +8,13 @@ def knapsack(weight, value, knapsack_weight, r):
     if r == -1:
         return 0
     
-    # Recursion: If current item's weight fits in knapsack
+    # 2 branch condition
     if weight[r] <= knapsack_weight:
-        # Take maximum of:
-        # 1. Include current item: value[r] + knapsack with reduced weight
-        # 2. Exclude current item: knapsack without this item
         return max(
             value[r] + knapsack(weight, value, knapsack_weight - weight[r], r - 1),
             knapsack(weight, value, knapsack_weight, r - 1)
         )
     
-    # Recursion: If current item is too heavy, skip it
+    # 1 branch condition
     if weight[r] > knapsack_weight:
         return knapsack(weight, value, knapsack_weight, r - 1)
